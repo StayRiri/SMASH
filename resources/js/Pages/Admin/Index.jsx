339ea96a9c530a8e5cb74e811aa1cafece91ad69
Riri <aries22ti@mahasiswa.pcr.ac.id>
2024-07-08 13:21:47 +0700

    Routing Login

---
+++ resources/js/Pages/Admin/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link, Head } from '@inertiajs/react';
+
+export default function Index() {
+    return(
+        <>
+            <Head title="Index" />
+            <h1>HALO TEST DL</h1>
+        </>
+    );
+}
